a2c1ae5533b1b8905eb8fe01b1887309cd6a9d66
Jimmir Miranda <87544734+Sugarylatics1@users.noreply.github.com>
2026-05-04 22:14:28 +0800

    h

--- app/src/main/java/com/example/campussafeapplication/SplashActivity.kt
+++ app/src/main/java/com/example/campussafeapplication/SplashActivity.kt
@@ -4,11 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.campussafeapplication.utils.SessionManager
 
 class SplashActivity : AppCompatActivity() {
@@ -22,16 +19,7 @@ class SplashActivity : AppCompatActivity() {
         }
 
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        
-        findViewById<android.view.View>(R.id.main)?.let { mainView ->
-            ViewCompat.setOnApplyWindowInsetsListener(mainView) { v, insets ->
-                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                insets
-            }
-        }
 
         // Delay for 3 seconds then decide which activity to start
         Handler(Looper.getMainLooper()).postDelayed({
